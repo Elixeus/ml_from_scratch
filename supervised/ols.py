21d6e8bf6b2f4941eea1580e0b9b5e01540680cf
Elixeus <xw876@nyu.edu>
2017-09-19 13:31:19 -0400

    change import

--- supervised/ols.py
+++ supervised/ols.py
@@ -3,7 +3,7 @@
 """The OLS class is built on top of the MLClass abstract class"""
 from __future__ import division
 import numpy as np
-from utils.utils import MLClass
+from ml_from_scratch.utils.utils import MLClass
 
 
 class OLS(MLClass):
